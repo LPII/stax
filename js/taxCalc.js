21c447b4e8cdb884f2dc9b11cfa0d4518c0f690a
LPII <Lanceportergd@gmail.com>
2016-06-05 01:56:48 -0400

    Added state tax values Added option selects

--- js/taxCalc.js
+++ js/taxCalc.js
@@ -1,34 +1,155 @@
 // Tax Calculator
 
-//Store user answers
-var itemCost = prompt("What is the cost of item?");
-var theState = prompt("What state do you live in?");
+
+//init function
+function taxCalc() {
+    //Store user answers
+    var itemCost = document.getElementById("theCost").value;
+    var theState = document.getElementById("stateSelect").value;
 
 
-//Init neccesary variables
-var salesTax; 
-var taxAmt;
-var subTotal;
+
+	//Init neccesary variables
+	var salesTax; 
+	var taxAmt;
+	var subTotal;
 
 
-//Check if number
-if (isNaN(itemCost)) 
-  {
-    alert("Must input numbers");
-    itemCost = prompt("What is the cost of item?");
+	//Check if number
+	if (isNaN(itemCost)) 
+	{
 
-  }
+		alert("Must input numbers");
+		itemCost = prompt("What is the cost of item?");
+
+	}
 
 
-//Check State and adjust tax accordingly
-if (theState == "alabama") {
+	//Check State and adjust tax accordingly
 
-	salesTax = .06
-  alert("bama");
+	if (theState == "Alaska" || "Delaware" || "Montana" || "New Hampshire" || "Oregon"){
+
+		salesTax = 1
+
+	}
+	else if (theState == "Arkansas" || "Kansas" || "Washington" ){
+
+		salesTax = .065
+
+	}
+	else if (theState == "Florida" || "Idaho" || "Iowa" || "Kentucky" || "Maryland" || "Michigan" || "Pennsylvania" || "Vermont" || "South Carolina" || "West Virginia"){
+
+		salesTax = .06
+
+	}
+	else if (theState == "Georgia" || "Hawaii" || "New York" || "Alabama" || "Wyoming"){
+
+		salesTax = .04
+
+	}
+	else if (theState == "Indiana" || "Mississippi" || "New Jersey" || "Rhode Island" || "Tennesse" ){
+
+		salesTax = .07
+
+	}
+	else if (theState == "Louisiana" || "North Dakota" || "Wisconsin" ){
+
+		salesTax = .05
+
+	}
+	else if (theState == "Maine" || "Nebraska" ){
+
+		salesTax = .055
+
+	}
+	else if (theState == "Oklahoma" || "South Dakota" ){
+
+		salesTax = .045
+
+	}
+	else if (theState == "Illinois" || "Texas" ){
+
+		salesTax = .06250
+
+	}
+	else if (theState == "Arizona" ){
+
+		salesTax = .056
+
+	}
+	else if (theState == "California" ){
+
+		salesTax = .075
+
+	}
+	else if (theState == "Colorado" ){
+
+		salesTax = .029
+
+	}
+	else if (theState == "Connecticut" ){
+
+		salesTax = .06350
+
+	}
+	else if (theState == "Nevada" ){
+
+		salesTax = .0685
+
+	}
+	else if (theState == "Massachusetts" ){
+
+		salesTax = .062
+
+	}
+	else if (theState == "Minnesota" ){
+
+		salesTax = .06875
+
+	}
+	else if (theState == "Missouri" ){
+
+		salesTax = .04225
+
+	}
+	else if (theState == "New Mexico" ){
+
+		salesTax = .05125
+
+	}
+	else if (theState == "North Carolina" ){
+
+		salesTax = .04750
+
+	}
+	else if (theState == "Ohio" ){
+
+		salesTax = .05750
+
+	}
+	else if (theState == "Utah" ){
+
+		salesTax = .047
+
+	}
+	else if (theState == "Virginia"){
+
+		salesTax = .043
+
+	}
+
+
+	//Perform Equation
+
+	taxAmt = itemCost * salesTax;
+
+	subTotal = taxAmt + itemCost;
+
+	alert(subTotal);
 }
-else{
 
-	salesTax=0
-  alert("not bama");
-  
-}
+
+// //write result in content div
+// document.getElementById("demo").innerHTML = taxCalc();
+
+
